9be865a7083125db1013e9b76b3ebd249b694b48
devsamuelmwangi <hirewebappdeveloper@gmail.com>
2023-06-13 21:48:23 +0300

    add solution

---
+++ debug.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import random
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import ipdb;
+from lib.models import Band, Venue, Concert
+
+
+if __name__ == '__main__':
+    
+    engine = create_engine('sqlite:///db/concerts.sqlite')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_band = Band(name="Imagine Dragons", hometown="Las Vegas, Nevada")
+    session.add(new_band)
+    session.commit()
+
+    new_venue = Venue(title="Allegiant Stadium", city="Las Vegas")
+    session.add(new_venue)
+    session.commit()
+
+    new_concert = Concert(date="2024-02-11", name="Super Bowl LVIII", band_id=new_band.id, venue_id=new_venue.id)
+    session.add(new_concert)
+    session.commit()
+    
+    bands = session.query(Band).all()
+    venues = session.query(Venue).all()
+    concerts = session.query(Concert).all()
+
+    for band in bands:
+        print(band.name)
+    
+    for venue in venues:
+        print(venue.title)
+
+    for concert in concerts:
+        print(concert.name)
+
+    ipdb.set_trace()
